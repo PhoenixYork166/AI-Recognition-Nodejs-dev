9f044a7efa1296ffb785543083b04a18ebf2cbc8
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-10-22 20:05:17 +0800

    prod

---
+++ util/database.js
@@ -0,0 +1,18 @@
+const rootDir = require('./path');
+require('dotenv').config({ path: `${rootDir}/.env`});
+
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        // host: process.env.POSTGRES_SERVICENAME,
+        host: process.env.POSTGRES_HOST,
+        user: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        port: process.env.POSTGRES_PORT
+    }
+});
+
+module.exports = db;
