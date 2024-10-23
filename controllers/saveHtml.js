3f2ca11892b26fc9a2138e3bebcd637f7b0958dc
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-10-23 17:37:57 +0800

    rootDir/controllers/saveHtml.js ok

--- controllers/saveHtml.js
+++ controllers/saveHtml.js
@@ -13,32 +13,60 @@ const saveHtml = async (req, res, puppeteer) => {
 
     try {
         const date = new Date().toISOString().replace(/:/g, '-');  // Format date for filename
+        // Define path for saving PDF to Node server
+        const pdfPath = path.join(__dirname, '..', 'user-pdf', `output_${date}.pdf`);
 
         // const browser = await puppeteer.launch();
         const browser = await puppeteer.launch({
+            headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox'] // Ensure Puppeteer runs in a safe environment if using Docker or any Linux-based server.
-        });
+        });      
+
         const page = await browser.newPage();
+
         await page.setContent(htmlContent);
-        // const pdfBuffer = await page.pdf({ format: 'A4' });
-        const pdfBuffer = await page.pdf();
+
+        // await page.setContent(htmlContent);
+        const pdfBuffer = await page.pdf({ 
+            format: 'A4', 
+            margin: {
+                top: '20px',
+                right: '20px',
+                bottom: '20px',
+                left: '20px'
+            },
+            landscape: true, 
+            printBackground: true 
+        });       
         
-        // Define path for saving PDF to Node server
-        const pdfPath = path.join(__dirname, '../user-pdf', `output_${date}.pdf`);
+        console.log(`\nSaving PDF to: ${pdfPath}\n`);
+        console.log(`\nGenerated PDF size:\n${pdfBuffer.length/1024} kb\n`);
         
         // Write PDF to a .pdf file in Node server
         await fs.writeFile(pdfPath, pdfBuffer);
         await browser.close();
 
         res.setHeader('Content-Type', 'application/pdf');
-        // res.contentType('application/pdf');
-        res.setHeader('Content-Disposition', `attachment; filename="color-details_${date}.pdf"`);
+        res.setHeader('Content-Disposition', `attachment; filename="${path.basename(pdfPath)}"`);
+
+        // Send the file
+        res.sendFile(pdfPath, (err) => {
+            if (err) {
+                console.error('File send failed:', err);
+                res.status(500).send('Error sending file!');
+            } else {
+                console.log('File sent successfully.');
+            }
+        });
+
+        // res.setHeader('Content-Type', 'application/pdf');
+        // res.setHeader('Content-Disposition', `attachment; filename="color-details_${date}.pdf"`);
         // res.setHeader('Content-Length', pdfBuffer.length);
-        res.send(pdfBuffer);
+        // res.send(pdfBuffer);
 
     } catch (err) {
         console.error(`\nError generating PDF: `, err, `\n`);
-        res.status(500).json({ status: { code: 500 }, error: err });
+        res.status(500).json({ status: { code: 500 }, error: `Error generating PDF: ${err}` });
     }
 };
 
