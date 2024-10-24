4d8f2b50d094269ac4aeffe51ef36cdafd9eb95e
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-10-25 05:18:55 +0800

    prod15

--- server.js
+++ server.js
@@ -44,22 +44,23 @@ const app = express();
 app.use(bodyParser.json({ limit: '100mb' }));
 /* Local dev Middleware for CORS (Cross-Origin-Resource-Sharing) */
 // const corsOptions = {
-//     origin: process.env.NODE_ENV === 'production' ? 'https://ai-recognition-frontend.onrender.com/' : 'http://localhost:3000',
+//     origin: process.env.NODE_ENV === 'production' ? 'https://ai-recognition-frontend.onrender.com' : 'http://localhost:3000',
 //     credentials: true, // to support session cookies
 //     methods: ['GET', 'POST', 'PUT', 'DELETE']
 //     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 // };
 
 // const corsOptions = {
-//     origin: process.env.NODE_ENV === 'production' ? 'https://ai-recognition-frontend.onrender.com/' : 'http://localhost:3000',
+//     origin: process.env.NODE_ENV === 'production' ? 'https://ai-recognition-frontend.onrender.com' : 'http://localhost:3000',
 //     credentials: true, // to support session cookies
 //     methods: ['GET', 'POST', 'PUT', 'DELETE']
 // };
 
 // app.use(cors(corsOptions));
-
-app.use(cors({credentials: true, origin: true}));
 //app.use(cors());
+
+//app.use(cors({credentials: true, origin: true}));
+app.use(cors({credentials: true, origin: 'https://ai-recognition-frontend.onrender.com'}));
 
 // Middleware for cookie-parser and pass the secret for signing the cookies
 app.use(cookieParser());
